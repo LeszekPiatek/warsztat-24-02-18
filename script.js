af06791039a6888217d45367fd58721a8c1e389d
LeszekPiatek <leszek.piatek@finanteq.com>
2018-02-24 12:59:52 +0100

    Funkcje

--- script.js
+++ script.js
@@ -1,54 +1,11 @@
-console.log('Hello Leszek!')
+//deklaracja funkcji, zadeklarowanie
 
-//primitives
-
-var textVariable = "Mateusz"
-
-var numberVariable = 555
-
-//true || false
-var boolVariable = true
-
-//zawsze null 
-var nullVariable = null
-
-//zawsze undefined
-var undefinedVariable = undefined
-
-console.log(textVariable)
-console.log(numberVariable)
-console.log(boolVariable)
-console.log(nullVariable)
-
-
-//objects
-
-var simplestObject = {
-    name: "Leszek",
-    lastname: "Piatek"
+function hello(){
+    console.log("Hello!")
+    console.log("Hello!")
+    console.log("Hello!")
 }
-console.log(simplestObject)
 
-var nestedObject = {
-    name: "car",
-    cartype: {
-        brand: "Audi",
-        type: "A6"
-    }
-}
-console.log(nestedObject)
-console.log(nestedObject.cartype.brand)
+//wywołanie funkcji, call, invocation
 
-
-//arrays (tablice)
-
-var simpleArray = [
-    1, 
-    2, {
-        tablicawtablicy: "a"
-    },
-    3, 
-    4, 
-    'tekst']
-console.log(simpleArray)
-console.log(simpleArray[4])
+hello()
