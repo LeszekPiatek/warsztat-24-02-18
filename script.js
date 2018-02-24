090bf1e4c5994fee34af73c50189e723c4bee4c9
LeszekPiatek <leszek.piatek@finanteq.com>
2018-02-24 12:28:44 +0100

    primitives (zmienne)

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('Hello Leszek!')
+
+//primitives
+
+var textVariable = "Mateusz"
+
+var numberVariable = 555
+
+//true || false
+var boolVariable = true
+
+//zawsze null
+var nullVariable = null
+
+//zawsze undefined
+var undefinedVariable = undefined
+
+console.log(textVariable)
+console.log(numberVariable)
+console.log(boolVariable)
+console.log(nullVariable)
