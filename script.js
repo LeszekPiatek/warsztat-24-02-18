21c5ff5bb59a463f007025284c1840cced62567a
LeszekPiatek <leszek.piatek@finanteq.com>
2018-02-24 12:50:38 +0100

    Obiekty

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var numberVariable = 555
 //true || false
 var boolVariable = true
 
-//zawsze null
+//zawsze null 
 var nullVariable = null
 
 //zawsze undefined
@@ -19,3 +19,36 @@ console.log(textVariable)
 console.log(numberVariable)
 console.log(boolVariable)
 console.log(nullVariable)
+
+
+//objects
+
+var simplestObject = {
+    name: "Leszek",
+    lastname: "Piatek"
+}
+console.log(simplestObject)
+
+var nestedObject = {
+    name: "car",
+    cartype: {
+        brand: "Audi",
+        type: "A6"
+    }
+}
+console.log(nestedObject)
+console.log(nestedObject.cartype.brand)
+
+
+//arrays (tablice)
+
+var simpleArray = [
+    1, 
+    2, {
+        tablicawtablicy: "a"
+    },
+    3, 
+    4, 
+    'tekst']
+console.log(simpleArray)
+console.log(simpleArray[4])
